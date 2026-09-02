0780beb1444d6254ada843006024873a59f81a59
Mahesh04022007 <anusurimahesh2007@gmail.com>
2026-09-02 10:44:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (42.72%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.py
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -1,13 +1,11 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        maxw=0
+        lst=[]
         for i in accounts:
-            s=0
-            for value in i:
-                s+=value
-            if s>maxw:
-                maxw=s
-        return maxw
-        
+            sm=0
+            for va in i:
+                sm+=va
+            lst.append(sm)
+        return max(lst)
 
         
